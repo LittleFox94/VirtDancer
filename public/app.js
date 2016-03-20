a5199ee0c795b9f8f7e0ef9d2762fd4d1d3edd2b
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2016-03-20 22:25:42 +0100

    show moar informationz
    also: default bootstrap theme

--- public/app.js
+++ public/app.js
@@ -41,4 +41,45 @@
 
         this.resetApp();
     }]);
+
+    // stolen from https://gist.github.com/thomseddon/3511330, thanks!
+    app.filter('bytes', function() {
+        return function(bytes, precision) {
+            if (isNaN(parseFloat(bytes)) || !isFinite(bytes) || bytes == 0) return '-';
+            if (typeof precision === 'undefined') precision = 1;
+            var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+                number = Math.floor(Math.log(bytes) / Math.log(1024));
+            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+        }
+    });
+
+    app.filter('nsDuration', function() {
+        return function(nanoseconds) {
+            if(nanoseconds == 0) return '-';
+
+            var units     = ['ns', 'µs', 'ms',  's', 'min', 'h', 'd'];
+            var factors   = [1,    1000, 1000, 1000,    60,  60,  24];
+            var tmps      = [0,       0,    0,     0,    0,   0,   0];
+            var output    = nanoseconds;
+            var unitIndex = 0;
+
+            while((output / factors[unitIndex + 1]) >= 1 && unitIndex < units.length) {
+                var oldValue    = output;
+                output          = Math.floor(output / factors[unitIndex + 1]);
+                tmps[unitIndex] = oldValue - (output * factors[unitIndex + 1]);
+                unitIndex++;
+            }
+
+            var outputString = output + units[unitIndex];
+            for(var i = unitIndex - 1; i >= 0; i--) {
+                if(tmps[i] == 0) {
+                    continue;
+                }
+
+                outputString += " " + tmps[i] + units[i];
+            }
+
+            return outputString;
+        }
+    });
 })();
